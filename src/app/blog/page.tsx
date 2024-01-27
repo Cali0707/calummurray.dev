9e77185c50ca4b2c4dec5d4b85914e18a5a50427
Cali0707 <calumramurray@gmail.com>
2024-01-27 18:09:10 -0500

    remove min height on blog page
    Signed-off-by: Cali0707 <calumramurray@gmail.com>

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
 
   return (
     <main
-      className={"flex min-h-screen flex-col items-center justify-start p-24"}
+      className={"flex flex-col items-center justify-start p-24"}
     >
       <h1
         className={

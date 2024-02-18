7d26201886192ddcff57d66e6cb53283a5b779d2
Cali0707 <calumramurray@gmail.com>
2024-02-18 17:09:02 -0500

    fix: don't import non existent stylesheet
    Signed-off-by: Cali0707 <calumramurray@gmail.com>

--- src/app/blog/blog-post-layout.tsx
+++ src/app/blog/blog-post-layout.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Image from "next/image";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import "@/styles/github-dark.css";
 import { ReportView } from "./view";
 import { kv } from "@vercel/kv";
 

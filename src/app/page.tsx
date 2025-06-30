809315c62f83d7e24fa735bd755072cbb9db34ec
Calum Murray <cmurray@redhat.com>
2025-06-30 14:28:21 -0400

    feat: updated blurb on home page
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,8 @@ export default function Home() {
           <br />
           <p>
             I am a Knative Eventing Maintainer and UX Lead, as well as a CNCF ambassador.
-            I am also an Engineering Science Student at the University of Toronto, where I major in
-            Electrical and Computer Engineering. My current research lies in LLMs for Software Engineering
-            tasks, as well as Event Driven and Serverless Systems. I am passionate about Open
-            Source, and I strongly believe in building in the open by default. I
+            My current research lies at the intersection of AI, Serverless, and Event Driven Systems. 
+	    I am passionate about Open Source, and I strongly believe in building in the open by default. I
             am interested in the intersection of algorithms, uncertainty,
             serverless computing, distributed systems, event driven systems, and
             machine learning.
@@ -58,7 +56,7 @@ export default function Home() {
             >
               Knative Eventing Kafka Broker
             </a>
-            , and &nbsp;
+            , and I lead &nbsp;
             <a
               href={"https://github.com/knative/ux/"}
               className={
@@ -72,6 +70,24 @@ export default function Home() {
             .
           </p>
           <br />
+	  <p>
+            I believe strongly in helping students participate in Open Source, and I co-founded the &nbsp;
+            <a
+              href={
+                "https://utoss.github.io/community/"
+              }
+              className={
+                "font-semibold transition-colors text-cyan-900 hover:text-cyan-600 dark:text-cyan-400 hover:dark:text-cyan-300 underline"
+              }
+              target={"_blank"}
+              rel={"noopener noreferrer"}
+            >
+	    University of Toronto Open Source Society (UTOSS)
+            </a>
+	    . Now that I have graduated, I am working to build upon my work at UTOSS to create resources
+	    to help other universities create their own Open Source Societies, as well as to create
+	    education resources to help students contribute to open source.
+	  </p
           <p>
             If you are interested in getting started contributing to open
             source, or you have an interesting open source project and would

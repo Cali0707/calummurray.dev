d2e35205f897d90120f17a733a32c6e2be072b9b
Calum Murray <cmurray@redhat.com>
2026-01-28 10:12:17 -0500

    feat: add references component to blog
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,13 @@
 import type { MDXComponents } from "mdx/types";
 
+type Reference = {
+  authors: string;
+  title: string;
+  venue?: string;
+  year: string | number;
+  url?: string;
+};
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <h1 className="text-2xl">{children}</h1>,
@@ -27,6 +35,35 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       >
         {children}
       </a>
+    ),
+    Cite: ({ num }: { num: number }) => (
+      <a
+        href={`#ref-${num}`}
+        className="text-cyan-900 hover:text-cyan-600 dark:text-cyan-400 hover:dark:text-cyan-300 underline text-sm align-super"
+      >
+        [{num}]
+      </a>
+    ),
+    References: ({ references }: { references: Reference[] }) => (
+      <ol className="list-decimal list-outside flex flex-col gap-2 pl-6 text-sm">
+        {references.map((ref, index) => (
+          <li key={index} id={`ref-${index + 1}`}>
+            {ref.authors}. "{ref.title}."
+            {ref.venue && <> <em>{ref.venue}</em>,</>} {ref.year}.
+            {ref.url && (
+              <>
+                {" "}
+                <a
+                  href={ref.url}
+                  className="text-cyan-900 hover:text-cyan-600 dark:text-cyan-400 hover:dark:text-cyan-300 underline"
+                >
+                  Link
+                </a>
+              </>
+            )}
+          </li>
+        ))}
+      </ol>
     ),
     ...components,
   };
